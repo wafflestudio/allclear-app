8e8503c0f52a888d3cf17cc10f2a7ef3be6f3030
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-12 14:31:04 +0900

    Chore/depedency upgrade (#3)
    * chore: update and remove deprecated packages
    
    * chore: update suffix of android build script
    
    * chore: upgrade lower-dependency packages
    
    * chore: update version code and name in build.gradle
    
    * feat: update tally link
    
    * chore: upgrade react-native to 0.77
    
    ---------
    
    Co-authored-by: cirtuare <cirtuare@gmail.com>

--- android/app/src/main/java/com/padocorp/clubhouse/MainApplication.kt
+++ android/app/src/main/java/com/padocorp/clubhouse/MainApplication.kt
@@ -9,18 +9,19 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.microsoft.codepush.react.CodePush
+import com.lugg.RNCConfig.RNCConfigPackage
+
 
 class MainApplication : Application(), ReactApplication {
  
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          val packages = PackageList(this).packages
+          packages.add(RNCConfigPackage())
+          return packages
         }
  
         override fun getJSMainModuleName(): String = "index"
@@ -38,11 +39,10 @@ class MainApplication : Application(), ReactApplication {
   
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
