77386d23dfb3344c49f3f1389b4bcccb88b08286
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-20 15:03:13 +0900

    Chore/dev pipeline setup (#8)
    * chore: remove separate APK build configuration from Gradle
    
    * chore: set up product flavors for development, staging, and production environments
    
    * chore: use 'dev' instead of 'local'
    
    * chore: remove OneSignal integration and related environment variable
    
    * chore: add environment configuration for development and set up CI workflow

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,7 @@
 import { Dimensions } from 'react-native'
-import { ENV } from './ENV'
 
 export const WINDOW_WIDTH = Dimensions.get('window').width
 export const WINDOW_HEIGHT = Dimensions.get('window').height
 
 export const initialDriverID = 0 // 0: 아무도 없음, 69:손명균, 170:원동준
 export const initialDriverAccountId = 4 // 4: 김지원
-
-export const ONESIGNAL_APP_ID = ENV.ONESIGNAL_APP_ID
