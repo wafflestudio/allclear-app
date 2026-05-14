1a04724ff2335bd94df604cbdf14b4621f868211
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-05-14 16:10:19 +0900

    refactor: update cache settings for queryClient and remove staleTime from useCategoryClubs

--- src/features/club/screens/ClubListScreen/index.tsx
+++ src/features/club/screens/ClubListScreen/index.tsx
@@ -78,8 +78,6 @@ const useCategoryClubs = ({ name, category }: UseCategoryClubsProps) => {
 		() => clubService.listClubs({ name, category }),
 		{
 			select: data => data.clubs,
-			staleTime: 60 * 1000,
-			cacheTime: 5 * 60 * 1000,
 		},
 	)
 }
